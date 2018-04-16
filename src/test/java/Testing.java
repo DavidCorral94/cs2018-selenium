5736c0be948a6d48b66d066c0cc28e5d66ede485
DavidCorral94 <david.corral@uca.es>
2018-04-16 09:20:41 +0200

    Minor fixes

--- src/test/java/Testing.java
+++ src/test/java/Testing.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class SeleniumTest {
+public class Testing {
     private static ChromeDriver driver;
     private WebElement element;
 
